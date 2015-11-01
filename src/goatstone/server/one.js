dbf7bc93a7caa22196ae90b18702524186102e58
joseherminiocollas <joseherminiocollas@goatstone.com>
2015-11-01 14:35:48 -0800

    Taking out mongodb, use a variable to store a todo.

--- src/goatstone/server/one.js
+++ src/goatstone/server/one.js
@@ -21,38 +21,27 @@ var bodyParser = require('body-parser');
 var multer = require('multer') 
 var upload = multer() 
 var cors = require('cors')
-var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
 var server 
+var todo = {}
 
-var url = 'mongodb://localhost:27017/test';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
-});
-
+// set up the server
 app.use( bodyParser.json() ) 
 app.use( bodyParser.urlencoded({ extended: true }) ) 
 app.use( cors() ) 
-
 app.use( express.static('public') ) 
+
 app.get( '/', function ( req, res ) {
     res.sendFile( fileRoot + 'index.html' ) 
 }) 
 app.post( '/todo', function ( req, res ) {
-	res.set( 'Access-Control-Allow-Origin', '*' )
-	//res.headers['Access-Control-Allow-Origin'] = '*';
-    console.log('post!', req.body )
-    //res.send('POST request to homepage') 
+    todo = req.body
 })
 app.get( '/todo', function( req, res ) {
-    // retrieve data and send to client
-	console.log('todo GET', res )
-	//res.setHeaders({'Access-Control-Allow-Origin':'*'})
-    res.send('GET request to homepage') 
+    res.status( 200 ).send( todo )
 })
+// implement the server
 server = app.listen( 3000, '192.168.1.7', function () {
     var host = server.address().address 
     var port = server.address().port 

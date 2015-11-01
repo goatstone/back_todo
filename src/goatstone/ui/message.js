6b2f9639d7195220be0b063580a2f2cc50d4f322
goat <goat@bauxite.local>
2015-10-31 17:18:07 -0700

    Set up models and UI interaction.

--- src/goatstone/ui/message.js
+++ src/goatstone/ui/message.js
@@ -5,31 +5,13 @@ var ReactDOM = require('react-dom');
 var Message = Backbone.View.extend({
   initialize: function(a) {
     this.message = [],
-    this.style = {};
+    this.style = { 'backgroundColor': 'azure' };
+    this.listenTo( this.model, 'change', this.render )
     return this
   },
-  set: function(msg) {
-    this.message = [ msg ]
-    this.render()
-    return this
-  },
-  append: function(msg){
-    this.message.push(msg)
-    this.render()
-    return this
-  },
-  render: function(msg) {
-    ReactDOM.render(<div style={this.style}>{  this.message.join(':::') } </div>, this.el)
+  render: function( msg ) {
+    ReactDOM.render(<div style={this.style}>{  this.model.get('text')  } </div>, this.el)
     return this 
-  },
-  setStyle: function(s){
-    this.style = s    
-    return this;
-  },
-  setColor: function(color){
-    this.style.color = color
-    this.render()
-    return this
-  }
+  } 
 });
 export default Message
